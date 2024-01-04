58703cbc884b86086461a99b56bd9ab56aa0af65
femmytedrey <femmytedrey@gmail.com>
2024-01-04 13:08:52 +0100

    about page added

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+      Embarking on a journey through the art of mixology, our dedicated craftsmen blend premium spirits and fresh, vibrant ingredients to create a symphony of flavors in each cocktail. With a passion for the alchemy of taste, our mixologists elevate libations into a sensory experience. From timeless classics to avant-garde elixirs, every creation is a testament to our commitment to the craft. Immerse yourself in the world of fine spirits, where each sip tells a story of innovation and tradition. Join us in celebrating the spirited convergence of creativity and expertise, where the cocktail becomes an extraordinary exploration of taste and pleasure. Cheers to the artistry of libation!
+      </p>
+    </section>
   )
 }
 
